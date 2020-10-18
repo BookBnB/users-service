65806372724f535133a7b796b5e11bcdcbe0259d
M-Picco <you@example.com>
2020-10-18 01:03:44 -0300

    Corrijo script de push a heroku

--- docker/heroku_push.sh
+++ docker/heroku_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./docker/build_production_image.sh
-docker tag node-template:latest registry.heroku.com/node-template-$TRAVIS_BRANCH/web
+docker tag users-service:latest registry.heroku.com/users-service-$TRAVIS_BRANCH/web
 echo "$HEROKU_API_KEY" | docker login --username=_ --password-stdin registry.heroku.com
-docker push registry.heroku.com/node-template-$TRAVIS_BRANCH/web
+docker push registry.heroku.com/users-service-$TRAVIS_BRANCH/web
 curl https://cli-assets.heroku.com/install.sh | sh  #install heroku
-heroku container:release web -a node-template-$TRAVIS_BRANCH
+heroku container:release web -a users-service-$TRAVIS_BRANCH

b35923cc0fa29da8680dcf6f1c9d9591db11d0aa
M-Picco <you@example.com>
2020-10-19 00:22:23 -0300

    Agrego base de datos sqlite para las pruebas

--- project/__init__.py
+++ project/__init__.py
@@ -3,9 +3,12 @@ from flask_migrate import Migrate
 from project.db import db
 from project.v1 import bp as bp_v1
 
-def create_app():
+def create_app(test_config = {}):
 	app = Flask(__name__)
 	app.config.from_object("project.config.Config")
+
+	if test_config is not None:
+		app.config.update(test_config)
 
 	db.init_app(app)
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,29 @@
+import os
 import pytest
+import tempfile
 
 from project import create_app
+from flask_migrate import upgrade
+
 
 @pytest.fixture
 def app():
     """Create and configure a new app instance for each test."""
+
+    db_fd, db_path = tempfile.mkstemp()
+
     # create the app with common test config
-    app = create_app()
+    app = create_app({
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///%s" % db_path
+    })
+
+    with app.app_context():
+        upgrade()
 
     yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
 
 
 @pytest.fixture

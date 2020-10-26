0ca6440945bad334da015ff17cfa73b429b0caa8
Franco Mazzoni <franco.mazzoni@despegar.com>
2020-10-25 21:38:11 -0300

    Change password max length

--- project/models/user.py
+++ project/models/user.py
@@ -5,7 +5,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
-    password = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(128), unique=True, nullable=False)
     new_field = db.Column(db.Integer, default=0, nullable=False)
     other_new_field = db.Column(db.Integer, default=0, nullable=False)

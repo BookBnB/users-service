f00409125c921524ef1eeb779150602913d923d2
Martin Picco <mpicco@fi.uba.ar>
2020-11-14 15:41:09 -0300

    Muevo definiciones de swagger a archivos yml

--- project/v1.py
+++ project/v1.py
@@ -1,70 +1,20 @@
 import datetime
 
 import jwt
+from flasgger import swag_from
 from flask import (Blueprint, Flask, current_app, jsonify, make_response,
                    request)
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from project.db import db
-from project.services.users_service import UserService
 from project.models.role import ROLES
+from project.services.users_service import UserService
 
 bp = Blueprint('v1', __name__, url_prefix='/v1')
 
 @bp.route('/users', methods=['POST'])
+@swag_from('swagger/users/post/users.yml')
 def users_create():
-    """Creación de Usuario
-    ---
-    parameters:
-      - name: body
-        in: body
-        required: true
-        schema:
-            $ref: '#/definitions/CrearUsuarioDTO'
-        required: true
-    definitions:
-      CrearUsuarioDTO:
-        type: object
-        properties:
-          email:
-            type: string
-          name:
-            type: string
-          surname:
-            type: string
-          password:
-            type: string
-          role:
-            type: string
-          phone:
-            type: string
-          city:
-            type: string
-      UsuarioDTO:
-        type: object
-        properties:
-          id:
-            type: string
-          email:
-            type: string
-          name:
-            type: string
-          surname:
-            type: string
-          password:
-            type: string
-          role:
-            type: string
-          phone:
-            type: string
-          city:
-            type: string
-    responses:
-      200:
-        description: Datos del usuario creado
-        schema:
-          $ref: '#/definitions/UsuarioDTO'
-    """
     body = request.get_json()
 
     try:
@@ -76,48 +26,14 @@ def users_create():
         return make_response({ 'message': str(ex) }, 400)
 
 @bp.route('/users', methods=['GET'])
+@swag_from('swagger/users/get/users.yml')
 def users_list():
-    """Listado de usuarios
-    ---
-    responses:
-      200:
-        description: Listado de usuarios
-        schema:
-          $ref: '#/definitions/UsuarioDTO'
-    """
     users = UserService().get_all()
     return jsonify([u.serialize() for u in users])
 
 @bp.route('/sessions', methods=['POST'])
+@swag_from('swagger/users/post/sessions.yml')
 def create_session():
-    """Crear una sesión para un usuario
-    ---
-    parameters:
-      - name: body
-        in: body
-        required: true
-        schema:
-            $ref: '#/definitions/CrearSesionDTO'
-        required: true
-    definitions:
-        CrearSesionDTO:
-            type: object
-            properties:
-                email:
-                    type: string
-                password:
-                    type: string
-        SesionDTO:
-            type: object
-            properties:
-                token:
-                    type: string
-    responses:
-        200:
-            description: Objeto con token de sesión creada
-            schema:
-                $ref: '#/definitions/SesionDTO'
-    """
     data = request.get_json()
 
     email = data.get('email', '')
@@ -147,20 +63,6 @@ def create_session():
     return make_response({ 'message': 'User not recognized' }, 401)
 
 @bp.route('/roles')
+@swag_from('swagger/users/get/roles.yml')
 def get_roles():
-    """Listado de roles
-    ---
-    definitions:
-        UsuarioDTO:
-            type: object
-            properties:
-                roles:
-                    type: array
-                    items: string
-    responses:
-        200:
-            description: Listado de roles
-            schema:
-                $ref: '#/definitions/UsuarioDTO'
-    """
     return jsonify({ 'roles': ROLES })

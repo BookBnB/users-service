f4b94d624eef15f4190f2fcfa206be5ed26ed26a
seba <sebastian.blazquez96@gmail.com>
2020-12-13 22:08:25 -0300

    Agrego test

--- tests/test_google_users.py
+++ tests/test_google_users.py
@@ -1,6 +1,7 @@
 import json
 import vcr
 from freezegun import freeze_time
+from base64 import b64decode
 
 def create_user(client, token):
     with vcr.use_cassette('tests/vcr_cassettes/google_token_certificate.yaml'):
@@ -35,3 +36,26 @@ def test_create_user_valid(client):
     assert res['email'] == 'sblazquez@fi.uba.ar'
     assert res['name'] == 'Sebastián Alejandro'
     assert res['surname'] == 'Blázquez Olivera'
+
+@freeze_time("2020-12-14 00:00:00Z")
+def test_login(client):
+    create_user(client, 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImUxOTdiZjJlODdiZDE5MDU1NzVmOWI2ZTVlYjQyNmVkYTVkNTc0ZTMiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI0MDc0MDg3MTgxOTIuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI0MDc0MDg3MTgxOTIuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTEwNjAyMDg1MDMxOTkzNTE2MTAiLCJoZCI6ImZpLnViYS5hciIsImVtYWlsIjoic2JsYXpxdWV6QGZpLnViYS5hciIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoibThhVV9KOFJydFEtUTk4NnNLWEgwQSIsIm5hbWUiOiJTZWJhc3Rpw6FuIEFsZWphbmRybyBCbMOhenF1ZXogT2xpdmVyYSIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS0vQU9oMTRHaTBTTEk3QWtzenVOWHNtd19FcnV0cnFldjRxSFNDTUpwZlMwbk49czk2LWMiLCJnaXZlbl9uYW1lIjoiU2ViYXN0acOhbiBBbGVqYW5kcm8iLCJmYW1pbHlfbmFtZSI6IkJsw6F6cXVleiBPbGl2ZXJhIiwibG9jYWxlIjoiZXMiLCJpYXQiOjE2MDc5MDM4MjIsImV4cCI6MTYwNzkwNzQyMn0.B7DOtaUKDFfKPyTugsAep8kCsXXl6S7QIux53K5DiYC9qKx37cP3kSN09h1mm6k-XPIKBnkng6NTEAafhzCvYUKF2grcyhQQpXkK-RR-hlbpn0KomShN6xzQiQYWpTXAjGAE9g-2ukHWO2MoWTudLFwV1mHBTQl2FnEwJ_QYdrZh_Kkdq-6-ycZJxW12LvH3n6lyGawIUN2ZizWjU8wiv5cs3YHgZYvwe0ufI9e8RoYVSwfb2dmSx_PH5Quo7I8EsIqcnNQQjlWZ48Ko3jQcYSKkdPMutxctuePTDggXI3cNZZLoaXla7QVrqrHUtfxjAQdqKn1cP3cyaKXYnXvjZQ')
+
+    res = client.post(path='/v1/sesiones/google', data=json.dumps({
+        'token': 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImUxOTdiZjJlODdiZDE5MDU1NzVmOWI2ZTVlYjQyNmVkYTVkNTc0ZTMiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI0MDc0MDg3MTgxOTIuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI0MDc0MDg3MTgxOTIuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTEwNjAyMDg1MDMxOTkzNTE2MTAiLCJoZCI6ImZpLnViYS5hciIsImVtYWlsIjoic2JsYXpxdWV6QGZpLnViYS5hciIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoibThhVV9KOFJydFEtUTk4NnNLWEgwQSIsIm5hbWUiOiJTZWJhc3Rpw6FuIEFsZWphbmRybyBCbMOhenF1ZXogT2xpdmVyYSIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS0vQU9oMTRHaTBTTEk3QWtzenVOWHNtd19FcnV0cnFldjRxSFNDTUpwZlMwbk49czk2LWMiLCJnaXZlbl9uYW1lIjoiU2ViYXN0acOhbiBBbGVqYW5kcm8iLCJmYW1pbHlfbmFtZSI6IkJsw6F6cXVleiBPbGl2ZXJhIiwibG9jYWxlIjoiZXMiLCJpYXQiOjE2MDc5MDM4MjIsImV4cCI6MTYwNzkwNzQyMn0.B7DOtaUKDFfKPyTugsAep8kCsXXl6S7QIux53K5DiYC9qKx37cP3kSN09h1mm6k-XPIKBnkng6NTEAafhzCvYUKF2grcyhQQpXkK-RR-hlbpn0KomShN6xzQiQYWpTXAjGAE9g-2ukHWO2MoWTudLFwV1mHBTQl2FnEwJ_QYdrZh_Kkdq-6-ycZJxW12LvH3n6lyGawIUN2ZizWjU8wiv5cs3YHgZYvwe0ufI9e8RoYVSwfb2dmSx_PH5Quo7I8EsIqcnNQQjlWZ48Ko3jQcYSKkdPMutxctuePTDggXI3cNZZLoaXla7QVrqrHUtfxjAQdqKn1cP3cyaKXYnXvjZQ',
+    }), content_type='application/json')
+
+    base64_data = res.get_json()['token'].split('.')[1]
+
+    missing_padding = len(base64_data) % 4
+    if missing_padding:
+        base64_data += (b'='* (4 - missing_padding)).decode()
+
+    decoded_data = b64decode(base64_data.encode()).decode()
+    data = json.loads(decoded_data)
+
+    assert res.status_code == 200
+    assert data['id']
+    assert data['email'] == 'sblazquez@fi.uba.ar'
+    assert data['role'] == 'host'
+    assert data['exp']

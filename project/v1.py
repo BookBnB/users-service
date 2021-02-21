60bd7a5d83c4161db56081f1bf32e0fb77701d3d
Martin Picco <mpicco@fi.uba.ar>
2021-02-21 15:11:55 -0300

    Validaciones adicionales

--- project/v1.py
+++ project/v1.py
@@ -66,11 +66,17 @@ def user_find(id, users: UserService):
 
 @bp.route('/usuarios/<id>/bloqueo', methods=['PUT'])
 @swag_from('swagger/users/put/users-block.yml')
-def block_user(id, users: UserService):
+def block_user(id, users: UserService, tokenizer: Tokenizer):
     try:
         user = users.get(id)
     except exc.DataError:
         return make_response({'message': 'Invalid id {}'.format(id)}, 400)
+
+    if not request_is_authenticated():
+        return make_response({'message': 'User not recognized'}, 401)
+
+    if not request_is_from_role('admin', tokenizer):
+        return make_response({'message': 'Only an admin can block users'}, 403)
 
     if not user:
         return make_response({'message': 'User with id {} does not exist'.format(id)}, 404)
@@ -163,6 +169,16 @@ def _generate_session_token(tokenizer, user):
         'exp': datetime.datetime.utcnow() + token_duration,
         'role': user.role
     }).decode('UTF-8')
+
+
+def request_is_authenticated():
+    return request.headers.get('Authorization') != None
+
+
+def request_is_from_role(role, tokenizer):
+    auth_header = request.headers.get('Authorization')
+    decoded_token = tokenizer.decode(auth_header.encode())
+    return decoded_token['role'] == role
 
 
 @bp.route('/roles')
--- tests/test_block_user.py
+++ tests/test_block_user.py
@@ -14,28 +14,35 @@ from tests.test_get_users import get_user
 import uuid
 
 
-def block_user(client, userId):
-    body = {'blocked': True}
+def create_user_and_login(client, role='host'):
+    admin = build_user(role=role, n=9)
+    create_user(client, admin)
+    status, res = login(client, admin)
+    return res['token']
+
+def block_request(client, userId, body, session_token=''):
     res = client.put(
         path='/v1/usuarios/' + userId + '/bloqueo',
         data=json.dumps(body),
-        content_type='application/json')
+        content_type='application/json',
+        headers=[('Authorization', session_token)] if session_token else []
+    )
     return res.status_code, json.loads(res.data.decode())
 
-def unblock_user(client, userId):
-    body = {'blocked': False}
-    res = client.put(
-        path='/v1/usuarios/' + userId + '/bloqueo',
-        data=json.dumps(body),
-        content_type='application/json')
-    return res.status_code, json.loads(res.data.decode())
+def block_user(client, userId, session_token=''):
+    return block_request(client, userId, {'blocked': True}, session_token)
+
+def unblock_user(client, userId, session_token=''):
+    return block_request(client, userId, {'blocked': False}, session_token)
 
 def test_block_user(client):
+    token = create_user_and_login(client, role='admin')
+
     user = build_user(role='guest')
     status, res = create_user(client, user)
     user_id = res['id']
 
-    status, res = block_user(client, user_id)
+    status, res = block_user(client, user_id, session_token=token)
 
     status, res = get_user(client, user_id)
 
@@ -43,11 +50,13 @@ def test_block_user(client):
     assert res['blocked'] == True
 
 def test_block_user_login(client):
+    token = create_user_and_login(client, role='admin')
+
     user = build_user(role='guest')
     status, res = create_user(client, user)
     user_id = res['id']
 
-    status, res = block_user(client, user_id)
+    status, res = block_user(client, user_id, session_token=token)
 
     status, res = login(client, user)
 
@@ -56,11 +65,13 @@ def test_block_user_login(client):
 
 @freeze_time(VALID_DATE)
 def test_block_user_google_login(client):
+    token = create_user_and_login(client, role='admin')
+
     user = build_user(role='guest')
     status, res = create_google_user(client, VALID_GOOGLE_TOKEN)
     user_id = res['id']
 
-    status, res = block_user(client, user_id)
+    status, res = block_user(client, user_id, session_token=token)
 
     status, res = google_login(client, VALID_GOOGLE_TOKEN)
 
@@ -68,12 +79,14 @@ def test_block_user_google_login(client):
     assert res['message'] == 'User is blocked'
 
 def test_block_unblock_user(client):
+    token = create_user_and_login(client, role='admin')
+
     user = build_user(role='guest')
     status, res = create_user(client, user)
     user_id = res['id']
 
-    status, res = block_user(client, user_id)
-    status, res = unblock_user(client, user_id)
+    status, res = block_user(client, user_id, session_token=token)
+    status, res = unblock_user(client, user_id, session_token=token)
 
     status, res = get_user(client, user_id)
 
@@ -82,6 +95,8 @@ def test_block_unblock_user(client):
 
 
 def test_block_user_invalid_id(client):
+    token = create_user_and_login(client, role='admin')
+
     user = build_user(role='guest')
     status, res = create_user(client, user)
 
@@ -91,12 +106,25 @@ def test_block_user_invalid_id(client):
     assert res['message'] == 'Invalid id aninvalidid'
 
 
-def test_block_user_invalid_id(client):
+def test_block_user_non_admin(client):
+    token = create_user_and_login(client, role='host')
+
+    user = build_user(role='guest')
+    status, res = create_user(client, user)
+    user_id = str(uuid.uuid4())
+
+    status, res = block_user(client, user_id, session_token=token)
+
+    assert status == 403
+    assert res['message'] == 'Only an admin can block users'
+
+
+def test_block_user_not_authenticated(client):
     user = build_user(role='guest')
     status, res = create_user(client, user)
     user_id = str(uuid.uuid4())
 
     status, res = block_user(client, user_id)
 
-    assert status == 404
-    assert res['message'] == 'User with id {} does not exist'.format(user_id)
+    assert status == 401
+    assert res['message'] == 'User not recognized'

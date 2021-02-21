ca8f475984dd569099a969b54d09fdfe6838e241
seblaz <sebastian.blazquez96@gmail.com>
2021-02-21 15:56:15 -0300

    Fix deploy

--- heroku/deploy.sh
+++ heroku/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker pull $IMAGE_NAME:$IMAGE_VERSION
-docker tag $IMAGE_NAME:$IMAGE_VERSION registry.heroku.com/bookbnb-$BRANCH/web
+docker tag $IMAGE_NAME:$IMAGE_VERSION registry.heroku.com/users-service-$BRANCH/web
 echo "$HEROKU_API_KEY" | docker login --username=_ --password-stdin registry.heroku.com
 docker push registry.heroku.com/users-service-$BRANCH/web
 curl https://cli-assets.heroku.com/install.sh | sh  # install heroku
